5d11a9828d1d16cd404a2b8692d9f338cc3ce062
Kedna Sousa <kednacleiasousa@hotmail.com>
2023-06-21 14:21:34 -0300

    Add files via upload

---
+++ aulasJs1/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,21 @@
+console.log("Trabalhando com atribuição de variáveis");
+//const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade + 1;
+
+
+"C:\Users\kedna\OneDrive\Área de Trabalho\javascript\projeto-atual\5-atribuicaoDeVariaveis.js"
